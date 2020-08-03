e05b52d7598486a30d5bde957e840e86a509cec2
martin <martin.majzel@inseko.sk>
2020-08-03 18:30:25 +0200

    first commit

---
+++ src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,12 @@
+package com.luv2code.ecommerce.config;
+
+import com.luv2code.ecommerce.entity.Product;
+import com.luv2code.ecommerce.entity.ProductCategory;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.http.HttpMethod;
+
+import java.net.URI;
+
+
